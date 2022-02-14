05f113fe086e5e321542e479991cd9fece3928e3
范士董 <fanshidong_1993@163.com>
2022-02-14 17:43:23 +0800

    init

---
+++ projects/plugin-project/loader/src/main/java/com/hzl/loader/sample/loader/SamplePluginLoader.java
@@ -0,0 +1,28 @@
+package com.hzl.loader.sample.loader;
+
+import android.content.Context;
+
+import com.tencent.shadow.core.loader.ShadowPluginLoader;
+import com.tencent.shadow.core.loader.managers.ComponentManager;
+
+/**
+ * 这里的类名和包名需要固定
+ * com.tencent.shadow.sdk.pluginloader.PluginLoaderImpl
+ */
+public class SamplePluginLoader extends ShadowPluginLoader {
+
+    private final static String TAG = "shadow";
+
+    private ComponentManager componentManager;
+
+    public SamplePluginLoader(Context hostAppContext) {
+        super(hostAppContext);
+        componentManager = new SampleComponentManager(hostAppContext);
+    }
+
+    @Override
+    public ComponentManager getComponentManager() {
+        return componentManager;
+    }
+
+}
---
+++ projects/plugin-project/runtime/src/main/java/com/hzl/runtime/PluginDefaultProxyActivity.java
@@ -0,0 +1,7 @@
+package com.hzl.runtime;
+
+
+import com.tencent.shadow.core.runtime.container.PluginContainerActivity;
+
+public class PluginDefaultProxyActivity extends PluginContainerActivity {
+}
